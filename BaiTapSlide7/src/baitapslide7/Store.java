a69a59ed8de4931a153975ab0742ca73ffa45512
unknown <cobangnho12@gmail.com>
2019-01-16 09:51:07 +0700

    bt

--- BaiTapSlide7/src/baitapslide7/Store.java
+++ BaiTapSlide7/src/baitapslide7/Store.java
@@ -60,39 +60,5 @@ public class Store{
         }
                 
     }
-    public static void main(String[] args) {
-        Store s = new Store();
-        Scanner sc = new Scanner(System.in);
-        int lc;
-         do{
-            System.out.println("===========MENU============");
-            System.out.println("1.Add Product");
-            System.out.println("2.List Product");
-            System.out.println("3.Seach");
-            System.out.println("4.Exit");
-            System.out.println("Ban chon: ");
-        //    System("cls");
-            lc = sc.nextInt();
-            switch(lc){
-                case 1:{
-                    s.start();
-                    break;
-                }
-                case 2:{
-                    s.prinInfo();
-                    break;
-                }
-                case 3:{
-                    System.out.println("Nhap ten ban muon tim kiem: ");
-                    sc.nextLine();
-                    String ten = sc.nextLine();
-                    s.search(ten);
-                    break;
-                }
-                case 4:
-                    break;
-            }
-      
-        }while(lc>0); 
-    }
+    
 }

be8b48e7431224e14d6b135672e8628c2508fb94
unknown <cobangnho12@gmail.com>
2019-02-16 14:36:37 +0700

    bai_tap

--- Exercise2_2/src/bosung/Bai3.java
+++ Exercise2_2/src/bosung/Bai3.java
@@ -4,14 +4,18 @@
  * and open the template in the editor.
  */
 package bosung;
-
+import java.util.Scanner;
 /**
  *
  * @author Thu Uyên
  */
 public class Bai3 {
     public static void main(String[] args) {
-       double tien = 3000000;
-       
+        Scanner sc = new Scanner(System.in);
+        int n = 3000000, a=100000, b=200000, c=500000;
+        System.out.printf("%d đổi thành %d tờ %d \n", n, n/a, a);
+        System.out.printf("%d đổi thành %d tờ %d \n", n, n/b, b);
+        System.out.printf("%d đổi thành %d tờ %d \n", n, n/c, c);
     }
+       
 }

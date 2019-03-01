9b2cc8ddacb0ac759b69380ae6648d47babd90c4
unknown <cobangnho12@gmail.com>
2019-03-01 23:21:48 +0700

    Bai_tap

---
+++ SinhVien/src/sinhvien/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sinhvien;
+
+/**
+ *
+ * @author Thu Uyên
+ */
+public class Main {
+    public static void main(String[] args) {
+        SinhVien sv = new SinhVien();
+        List l = new List();
+        l.start();
+        l.xuatds();
+        l.DSSV();
+    }
+}

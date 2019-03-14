0cbace32d3fac892c1c8ba061978dce90eaadaec
unknown <cobangnho12@gmail.com>
2019-03-15 00:46:52 +0700

    Bai_tap

--- Buoi7_class/Polylomia_Bai10/src/polylomia_bai10/Polylomia.java
+++ Buoi7_class/Polylomia_Bai10/src/polylomia_bai10/Polylomia.java
@@ -5,23 +5,53 @@
  */
 package polylomia_bai10;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Thu Uyên
  */
 public class Polylomia {
-    
+    private int[] a;
+
+    public Polylomia() {
+    }
+
+    public Polylomia(int[] a) {
+        this.a = a;
+    }
+
+    public int[] getA() {
+        return a;
+    }
+
+    public void setA(int[] a) {
+        this.a = a;
+    }
+    public void Nhap(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập đa thức bậc: ");
+        int n = sc.nextInt();
+       
+        for (int i = 0; i < n; i++) {
+            System.out.println("Nhập hệ số của x mũ " + i + " là: ");
+            a[i] = sc.nextInt();
+        }
+    }
+   
+  
     public static void main(String[] args) {
-//        int[] a;//mảng chứa đa thuwscc 1
-//        int[] b;// mảng chứa đa thức 2
-//        int[] c;// chứa mảng tích
-//        for (int i = 0; i < a.length; i++) {
-//            for (int j = i; j >=0 ; j--) {
-//                c[i] += a[j]*b[i-j];
-//                
-//            }
-//            
-//        }
+       Scanner sc = new Scanner(System.in);
+       Polylomia p1 = new Polylomia();
+       p1.Nhap();
+       Polylomia p2= new Polylomia();
+       p2.Nhap();
+       int[] c = {0};// chứa mảng tích
+        for (int i = 0; i < p1.a.length; i++) {
+            for (int j = i; j >=0 ; j--) {
+                c[i] += p1.a[j]* p2.a[i-j];
+            }
+        }
         // TODO code application logic here
     }
     

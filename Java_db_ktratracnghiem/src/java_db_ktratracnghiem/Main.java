4c430835a5d54737eb57d9c3416e01d2184ba2fa
unknown <cobangnho12@gmail.com>
2019-06-07 22:22:52 +0700

    bt_bt

--- Java_db_ktratracnghiem/src/java_db_ktratracnghiem/Main.java
+++ Java_db_ktratracnghiem/src/java_db_ktratracnghiem/Main.java
@@ -8,6 +8,7 @@ package java_db_ktratracnghiem;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -23,46 +24,68 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        try {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("    QUIZ ONLINE      ");
-            System.out.println("---------------------");
-            System.out.print("Enter email: ");
-            String email = sc.nextLine();
-            System.out.print("Enter pass: ");
-            String pass = sc.nextLine();
-            StudentManager stm = new StudentManager();
-            Student s = stm.login(email, pass);
-            if(s != null){
+try {
+            Scanner input = new Scanner(System.in);
+            System.out.println("   QUIZ ONLINE   ");
+            System.out.println("------------------");
+            System.out.print("Enter Email: ");
+            String email = input.nextLine();
+
+            System.out.print("Enter Pass: ");
+            String pass = input.nextLine();
+
+            StudentManager sm = new StudentManager();
+            Student s = sm.login(email, pass);
+
+            if (s != null) {
                 System.out.println("LOGIN SUCCESS! PRESS ANY KEY TO START QUIZ");
-                sc.nextLine();
-                System.out.println("------------------------");
-                QuestionManager qm = new QuestionManager();
-                List<Questions> questions = qm.getQuestions();
-                int countCorr = 0;
-                int index = 1;
-                for (Questions q : questions) {
-                    System.out.printf("Question %d/%d", index++, questions.size());
-                    System.out.println("");
-                    System.out.println(q);
-                    System.out.print("Your Answer > ");
-                    String answer = sc.nextLine();
-                    stm.addAnswer(s, q, answer);
-                    if(answer.toLowerCase().equals(q.getCorrect().toLowerCase())){
-                        countCorr++;
+                input.nextLine();
+                boolean completed = sm.isCompleted(s);
+                if (completed) {
+                    System.out.println("YOU HAVE COMPLETED THE TEST.");
+                    System.out.print("DO YOU WANT TO DO IT AGAIN (Y/N): ");
+                    String cont = input.nextLine();
+                    if (cont.toLowerCase().equals("y")) {
+                        sm.resetQuiz(s);
+                        startQuiz(input, sm, s);    
                     }
+                } else {
+                    startQuiz(input, sm, s);
                 }
-                System.out.println("Congratulation! ");
-                System.out.printf("Your result: %d/%d", countCorr, questions.size());
-                System.out.println("See Detail(Y/N)");
-                stm.printResult();
-            }else{
-                System.out.println("LOGIN FAIL");
+            } else {
+                System.out.println("LOGIN FAIL!");
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
-        
     }
-    
+
+    public static void startQuiz(Scanner input, StudentManager sm, Student s) throws ClassNotFoundException, SQLException {
+        System.out.println("---------------");
+        QuestionManager qm = new QuestionManager();
+        List<Questions> questions = qm.getQuestions();
+        int countCorr = 0;
+        int index = 1;
+        for (Questions q : questions) {
+            System.out.printf("Question %d/%d \n", index++, questions.size());
+            System.out.println(q);
+            System.out.print("Your answer > ");
+            String answer = input.nextLine();
+            sm.addAnswer(s, q, answer);
+            if (answer.toLowerCase().equals(q.getCorrect().toLowerCase())) {
+                countCorr++;
+            }
+        }
+        System.out.println("Congratulation!");
+        System.out.printf("Your result: %d/%d \n", countCorr, questions.size());
+        System.out.print("See Detail (Y/N) > ");
+         String cont = input.nextLine();
+        if (cont.toLowerCase().equals("y")) {
+            sm.printResult(s);
+        }
+    }
+
 }
+       
+
--- Java_db_ktratracnghiem/src/java_db_ktratracnghiem/StudentManager.java
+++ Java_db_ktratracnghiem/src/java_db_ktratracnghiem/StudentManager.java
@@ -9,38 +9,104 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 /**
  *
  * @author Thu Uyên
  */
 public class StudentManager {
+
     Connection conn;
-    public StudentManager() throws ClassNotFoundException, SQLException{
+
+    public StudentManager() throws ClassNotFoundException, SQLException {
         DBconnector db = new DBconnector();
         this.conn = db.getConnect();
     }
-    public Student login(String email, String pass) throws SQLException{
-         String query = "SELECT * FROM students WHERE email = ? AND pass = ?";
-         PreparedStatement pstmt =this.conn.prepareStatement(query);
-         pstmt.setString(1, email);
-         pstmt.setString(2, pass);
-         ResultSet rs = pstmt.executeQuery();
-         Student s = null;
-         if(rs.next()){
-             s = new Student(rs.getInt("id"),rs.getString("name") ,rs.getString("email"), rs.getString("pass"));
-         }
+
+    public Student login(String email, String pass) throws SQLException {
+        // 1. Taoj ket noi
+        // 2. Thuc hien cau lenh
+        String query = "SELECT * FROM students WHERE email = ? AND pass = ?";
+        PreparedStatement pstmt = this.conn.prepareStatement(query);
+        pstmt.setString(1, email);
+        pstmt.setString(2, pass);
+
+        ResultSet rs = pstmt.executeQuery();
+        Student s = null;
+        if (rs.next()) {
+            s = new Student(rs.getInt("id"), rs.getString("name"),
+                    rs.getString("email"), rs.getString("pass"));
+        }
+
         return s;
     }
-    public int addAnswer(Student s, Questions q, String answer) throws SQLException{
-        String query = "INSELT INTO student_question VALUE (?,?,?)";
+    
+    public boolean isCompleted(Student s) throws SQLException {
+        String query = "SELECT * FROM student_question WHERE student_id = ?";
+        PreparedStatement pstmt = this.conn.prepareStatement(query);
+        pstmt.setInt(1, s.getId());
+        ResultSet rs = pstmt.executeQuery();
+        return rs.next();
+    }
+    
+    public void resetQuiz(Student s) throws SQLException {
+        String query = "DELETE FROM student_question WHERE student_id = ?";
+        PreparedStatement pstmt = this.conn.prepareStatement(query);
+        pstmt.setInt(1, s.getId());
+        pstmt.executeUpdate();
+    } 
+
+    public int addAnswer(Student s, Questions q, String answer) throws SQLException {
+        String query = "INSERT INTO student_question VALUES (?,?,?)";
         PreparedStatement pstmt = this.conn.prepareStatement(query);
         pstmt.setInt(1, s.getId());
         pstmt.setInt(2, q.getId());
         pstmt.setString(3, answer);
         return pstmt.executeUpdate();
     }
-    public  void printResult(){
+
+    public void printResult(Student s) throws SQLException, ClassNotFoundException {
+
+        String query = "select sq.correct as student_answer, q.* from student_question as sq "
+                + "join questions as q on q.`id` = sq.`question_id` "
+                + "where sq.`student_id` = ?";
+        PreparedStatement pstmt = this.conn.prepareStatement(query);
+        pstmt.setInt(1, s.getId());
+        ResultSet rs = pstmt.executeQuery();
+        int questionCount = 1;
         
+        QuestionManager qm = new QuestionManager();
+        List<Questions> questions = qm.getQuestions();
+        while (rs.next()) {
+            // so sanh ket qua
+            String result = rs.getString("correct").toLowerCase()
+                    .equals(rs.getString("student_answer").toLowerCase()) 
+                    ? "True" : "False";
+            System.out.printf("Question %d/%d - %s\n", questionCount++, questions.size() ,result);
+            System.out.println(rs.getString("content"));
+            
+            if (rs.getString("correct").equals("a"))
+                System.out.println("[A]. " + rs.getString("answer_a"));
+            else 
+                System.out.println("A. " + rs.getString("answer_a"));
+            
+            if (rs.getString("correct").equals("b"))
+                System.out.println("[B]. " + rs.getString("answer_b"));
+            else 
+                System.out.println("B. " + rs.getString("answer_b"));
+            
+            if (rs.getString("correct").equals("c"))
+                System.out.println("[C]. " + rs.getString("answer_c"));
+            else 
+                System.out.println("C. " + rs.getString("answer_c"));
+            
+            if (rs.getString("correct").equals("d"))
+                System.out.println("[D]. " + rs.getString("answer_d"));
+            else 
+                System.out.println("D. " + rs.getString("answer_d"));
+            
+            
+        }
     }
 }
